21864525f2dd040d573e56e1801401b030535d8b
nuuuwan <nuuuwan@gmail.com>
2025-02-26 12:53:26 +0530

    blank in config

--- src/common/ConfigFile.py
+++ src/common/ConfigFile.py
@@ -3,7 +3,7 @@ class ConfigFile:
         self.file_path = file_path
 
     def write(self, data: dict):
-        content = "\n".join([f"{key}={value}" for key, value in data.items()])
+        content = "\n".join([f"{key} = {value}" for key, value in data.items()])
         with open(self.file_path, "w") as file:
             file.write(content)
         print(f"Wrote {len(data)} key-value pairs to {self.file_path}")
